63d705848a9addf4da05095f75b3872c4e70e4ec
sijos <ssny88@gmail.com>
2017-04-03 15:41:43 -0700

    Create tile component, space holder for now

---
+++ frontend/components/tile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Tile extends React.Component {
+
+  render() {
+    return(
+      <div className="tile-off">Tile Space</div>
+    );
+  }
+}
+
+export default Tile;
