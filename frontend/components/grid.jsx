2f848ca5364ba3fc7be010a4023fa6ba69a15cb3
sijos <ssny88@gmail.com>
2017-04-02 18:43:50 -0700

    create and render grid component shell

---
+++ frontend/components/grid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Grid extends React.Component {
+
+
+  render() {
+    return(
+      <div>Space for Grid</div>
+    )
+  }
+}
+
+export default Grid;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Grid from './components/grid';
+import Grid from './components/grid';
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById('root');
-  ReactDOM.render(<h2>Working Grid Here</h2>, root);
+  ReactDOM.render(<Grid />, root);
 });
