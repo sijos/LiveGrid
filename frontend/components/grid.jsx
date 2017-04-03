b7ded0b16ebcadcf45dca251a0c35ea411ed9cda
sijos <ssny88@gmail.com>
2017-04-03 15:43:17 -0700

    Render initial version of grid

--- frontend/components/grid.jsx
+++ frontend/components/grid.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Tile from './tile';
 
 class Grid extends React.Component {
 
 
+  renderRow(colId) {
+    const row = Array.from(Array(16).keys());
+    row.map(rowId => <Tile colId={colId} rowId={rowId}
+                           key={`${rowId}, ${colId}`} />);
+    return (
+      <div className="grid-row" key={colId}>
+        { row }
+      </div>
+    );
+  }
+
   render() {
+    const cols = Array.from(Array(16).keys());
+    const grid = cols.map(colId => this.renderRow(colId));
     return(
-      <div>Space for Grid</div>
-    )
+      <div>{grid}</div>
+    );
   }
 }
 
