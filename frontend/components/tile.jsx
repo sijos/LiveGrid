e9290d4c97ce3f93cddc2df1b2d4ed1e9fc54392
sijos <ssny88@gmail.com>
2017-04-04 20:29:51 -0700

    setup state for tiles and change CSS class onclick

--- frontend/components/tile.jsx
+++ frontend/components/tile.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: "off" };
+    this.toggleStatus = this.toggleStatus.bind(this);
+  }
+
+  toggleStatus(e) {
+    e.preventDefault();
+    const status = this.state.status === "off" ? "on" : "off";
+    this.setState({ status });
+  }
   
   render() {
     return(
-      <div className="tile-off">[ ]</div>
+      <div className={`tile ${this.state.status}`}
+           onClick={this.toggleStatus}>[ ]
+      </div>
     );
   }
 }
