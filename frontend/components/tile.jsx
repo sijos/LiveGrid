3a3acaec945262ea4812830fd049ef43b14b0ec2
sijos <ssny88@gmail.com>
2017-04-11 16:33:32 -0700

    remove unnecessary code from tile class

--- frontend/components/tile.jsx
+++ frontend/components/tile.jsx
@@ -5,23 +5,10 @@ import { timeStarts, synthNotes } from './constants';
 class Tile extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = { status: "off" };
     this.toggleStatus = this.toggleStatus.bind(this);
   }
 
   toggleStatus() {
-    // let status;
-    
-    // const time = timeStarts[this.props.colId];
-    // const note = synthNotes[this.props.rowId];
-    // if (this.state.status === "off") {
-    //   status = "on";
-    //   this.props.part.add(time, note);
-    // } else {
-    //   status = "off";
-    //   this.props.part.remove(time, note);
-    // }
-    // this.setState({ status });
     this.props.toggleTile([this.props.rowId, this.props.colId]);
   }
   
