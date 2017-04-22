ef61b86af1ac711ba336d333e252f505a9ae6171
Scott Snyder <ssny88@gmail.com>
2017-04-22 14:16:24 -0700

    move widget generation into controls & setup working tempo slider

--- frontend/components/controls.jsx
+++ frontend/components/controls.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tone from 'tone';
-import { panel, slider } from '../../widgets';
+import Interface from '../../build/interface';
 
 class Controls extends React.Component {
   constructor(props) {
@@ -10,11 +10,27 @@ class Controls extends React.Component {
     };
   }
 
-  // componentWillMount() {
-  //   console.log(panel);
-  //   console.log(slider);
-  //   panel.add(slider);
-  // }
+  componentDidMount() {
+    let panel = new Interface.Panel({
+      useRelativeSizesAndPositions: true
+    });
+
+    let slider = new Interface.Slider({
+      isVertical: false,
+      bounds: [0, 0, .1, .2],
+      min: 80,
+      max: 200,
+      value: 120,
+      onvaluechange: () => {
+        if (slider.value) {
+          this.setState({ bpm: slider.value });
+          Tone.Transport.bpm.value = slider.value;
+        }
+      }
+    });
+
+    panel.add(slider);
+  }
 
   play() {
     if (Tone.Transport.state === "started") {
@@ -32,6 +48,11 @@ class Controls extends React.Component {
     // Tone.Master.chain(dist);
   }
 
+  stateLog() {
+    console.log(this.state);
+    console.log(Tone.Transport.bpm.value);
+  }
+
   render() {
     return (
       <div className="control-panel">
@@ -42,6 +63,7 @@ class Controls extends React.Component {
                 onClick={this.props.clearGrid}>Clear Grid
         </button>
         <button onClick={this.fxTest.bind(this)}>FX</button>
+        <button onClick={this.stateLog.bind(this)}>State</button>
       </div>
     );
   }
